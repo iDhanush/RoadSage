d47a40f795458d5fac38e17914953582f029a904
iDhanush <idhanushm@gmail.com>
2025-02-05 01:24:32 +0530

    database ini

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import datetime
 from server import app
+from global_var import Var
+from schemas import HoleReport
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get('/report_hole')
-async def report_hole():
-    pass
+
+@app.post('/report_hole')
+async def report_hole(hole_report: HoleReport):
+    await Var.db.add_hole_report(hole_report.lon, hole_report.lat, datetime.datetime.now(), hole_report.locname)
+    return {"message": "success"}
+
+
+@app.post('/report_crash')
+async def report_crash(hole_report: HoleReport):
+    await Var.db.add_crash_report(hole_report.lon, hole_report.lat, datetime.datetime.now(), hole_report.locname)
+    return {"message": "success"}

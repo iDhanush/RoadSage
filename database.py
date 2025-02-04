d058b5190bcdf07a9a4875c9b8f5bd94bf62fbd8
iDhanush <idhanushm@gmail.com>
2025-02-05 01:11:05 +0530

    report database

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import motor.motor_asyncio
 
@@ -8,4 +9,14 @@ class Files_Database:
         self._client = motor.motor_asyncio.AsyncIOMotorClient(_uri)
         self.db = self._client['roadsage']
         self.holeDB = self.db['holeDB']
-    async def add_hole_report(self):
+        self.crashDB = self.db['crashDB']
+
+    async def add_hole_report(self, lon: float, lat: float, time: datetime.datetime, locname: str):
+        await self.holeDB.insert_one({'lon': lon, 'lat': lat, 'time': time, 'locname': locname})
+
+    async def get_hole_report(self):
+        await self.holeDB.find({}).sort('time', -1).to_list(None)
+
+    async def add_crash_report(self, lon: float, lat: float, time: datetime.datetime, locname: str):
+        await self.crashDB.insert_one({'lon': lon, 'lat': lat, 'time': time, 'locname': locname})
+
